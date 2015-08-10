b4119bd5d66bedb1b161d2a9a11b4ed5f56dbfec
afun <endlessproof@gmail.com>
2015-08-10 13:34:50 +0800

    ADD: Install Hopper

--- tool/README.md
+++ tool/README.md
@@ -1,3 +1,4 @@
 # Including
 * [rsatool](https://github.com/ius/rsatool): rsatool calculates RSA (p, q, n, d, e) and RSA-CRT (dP, dQ, qInv) parameters given either two primes (p, q) or modulus and private exponent (n, d).
 * [pkcrack](https://www.unix-ag.uni-kl.de/~conrad/krypto/pkcrack.html): The attack is a known plaintext attack, which means you have to know part of the encrypted data in order to break the cipher.
+* [Hopper](http://www.hopperapp.com/):  Hopper is a reverse engineering tool for OS X and Linux
--- tool/config.sh
+++ tool/config.sh
@@ -17,3 +17,17 @@ echo "Installing pkcrack"
 cp current_path/pkcrack-1.2.2.tar.gz .
 tar zxvf pkcrack-1.2.2.tar.gz
 rm pkcrack-1.2.2.tar.gz
+
+echo -n ""
+echo "##########"
+echo "Installing Hopper"
+sudo echo "## Append by linux_env_config/tool" >> /etc/apt/source.list
+UBUNTU_BITS_VERSION=$(uname -m)
+if [ "$UBUNTU_BITS_VERSION" == "i686" ];then
+  sudo echo "deb http://www.hopperapp.com/linux/i386 repo/" >> /etc/apt/source.list
+else
+  sudo echo "deb http://www.hopperapp.com/linux/amd64 repo/" >> /etc/apt/source.list
+fi
+sudo apt-key adv --recv-keys --keyserver keyserver.ubuntu.com E9EB04D3
+sudo apt-get update
+sudo apt-get install hopper
