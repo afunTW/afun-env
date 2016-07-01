82831d5e81b79738ad05c9ba93cb36d82a7505ba
afunTW <endlessproof>
2016-07-01 13:45:53 +0800

    modify: set zsh and agnoster theme as default

--- ubuntu14.04/setZsh.sh
+++ ubuntu14.04/setZsh.sh
@@ -1,3 +1,6 @@
 sudo apt-get update
 sudo apt-get install zsh
 sh -c "$(wget https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh -O -)"
+chsh -s /bin/zsh
+cp .zshrc ~/
+zsh
