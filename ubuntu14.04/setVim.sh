e7981543c949a0c69529d3c11b73647abb0b5e24
afunTW <endlessproof>
2016-07-01 13:38:40 +0800

    modify: install powerline

--- ubuntu14.04/setVim.sh
+++ ubuntu14.04/setVim.sh
@@ -1,13 +1,9 @@
 # setting vim vundle
-rm -rf ~/.vim/
-rm ~/.vimrc
-# ln -s .vimrc ~/
-# ln -s .vim ~/
 cp -r .vim ~/
 cp .vimrc ~/
 vim +PluginInstall +qall
 
 # setting powerline
-cp -r .fonts ~/	
-fc-cache -vf ~/.fonts/
-cp -r conf.d/ ~/.config/fontconfig/
+sudo apt-get install git
+git clone https://github.com/powerline/fonts ~/
+./.fonts/install.sh
