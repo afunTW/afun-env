65f4531e583c62a96a387b4c965847c06831e5eb
afunTW <endlessproof@gmail.com>
2019-04-03 15:09:06 +0800

    remove useless script

--- R/R-xenial.sh
+++
@@ -1,9 +0,0 @@
-sudo echo "deb http://cran.rstudio.com/bin/linux/ubuntu xenial/" | sudo tee -a /etc/apt/sources.list
-gpg --keyserver keyserver.ubuntu.com --recv-key E084DAB9
-gpg -a --export E084DAB9 | sudo apt-key add -
-sudo apt-get update
-sudo apt-get install r-base r-base-dev
-sudo apt-get install gdebi-core
-wget https://download1.rstudio.org/rstudio-0.99.896-amd64.deb
-sudo gdebi -n rstudio-0.99.896-amd64.deb
-rm rstudio-0.99.896-amd64.deb
