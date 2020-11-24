9f816ce17f8f9debe111e8eb2de3e913d6f23137
afunTW <endlessproof@gmail.com>
2020-11-24 15:40:34 +0800

    update

---
+++ devtool/neovim/setup.sh
@@ -0,0 +1,3 @@
+# install vim plug to manage plugin
+sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
+       https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
