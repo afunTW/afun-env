0048257fd62fabf473801c2ecf41bf0fe68b8325
afunTW <endlessproof@gmail.com>
2020-11-28 01:18:01 +0800

    update

--- setup.sh
+++ setup.sh
@@ -1,7 +1,9 @@
 #!/usr/bin/bash
 
 sudo apt update
-sudo apt install -y git curl tree byobu
+sudo apt install -y \
+    git curl tree byobu \
+    powerline fonts-powerline
 
 ./devtools/setup_alternative.sh
 ./devtools/setup_gitconfig.sh
