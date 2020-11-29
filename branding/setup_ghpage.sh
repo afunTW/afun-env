26ebca342d46bffa1b2648b70588bd43d513ea5a
afunTW <endlessproof@gmail.com>
2020-11-29 09:37:38 +0800

    add

---
+++ branding/setup_ghpage.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/bash
+
+BRANDING_DIR="$HOME/workspace/branding"
+mkdir -p $BRANDING_DIR
+
+# remember to setup up ssh auth
+ssh -T git@github.com
+
+# blog
+git clone git@github.com:afunTW/afunTW.github.io.git "$BRANDING_DIR/afunTW.github.io"
+npm install -g hexo-cli
+
+# resume
+git clone git@github.com:afunTW/cv.git "$BRANDING_DIR/cv"
+
+# github profile
+git clone git@github.com:afunTW/afunTW.git "$BRANDING_DIR/afunTW"
