e45dc0a2c780774bcc40bf768b7b553e79e56527
afunTW <endlessproof@gmail.com>
2020-11-29 09:38:44 +0800

    update

--- setup.sh
+++ setup.sh
@@ -1,10 +1,21 @@
 #!/usr/bin/bash
 
+# ##############################
+# System
+# ##############################
 sudo apt update
 sudo apt install -y \
     git curl tree byobu \
     powerline fonts-powerline
 
+# ##############################
+# NodeJs
+# ##############################
+sudo apt install -y nodejs npm
+
+# ##############################
+# Devtools
+# ##############################
 ./devtools/setup_alternative.sh
 ./devtools/setup_gitconfig.sh
 ./devtools/setup_zshrc.sh
