1f1c185e68a0bd765a7bffb6cd793f8d2a0d7692
afunTW <endlessproof@gmail.com>
2020-12-01 11:48:39 +0800

    update

--- setup.sh
+++ setup.sh
@@ -25,7 +25,8 @@ sudo apt install -y nodejs npm
 # ##############################
 sudo apt-get install -y make build-essential libssl-dev zlib1g-dev \
 libbz2-dev libreadline-dev libsqlite3-dev wget curl llvm libncurses5-dev \
-libncursesw5-dev xz-utils tk-dev libffi-dev liblzma-dev python-openssl pipenv
+libncursesw5-dev xz-utils tk-dev libffi-dev liblzma-dev python-openssl \
+pipenv ipython3
 
 curl https://pyenv.run | bash
 
