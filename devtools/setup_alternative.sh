0a3ddc906500f5966f92314363eb428bbe6ebb1e
afunTW <endlessproof@gmail.com>
2020-12-01 09:53:12 +0800

    update: alternative df

--- devtools/setup_alternative.sh
+++ devtools/setup_alternative.sh
@@ -1,13 +1,15 @@
 #!/usr/bin/bash
 
-# alternative cat - bat
+# cat -> bat
 # https://github.com/sharkdp/bat/
 sudo apt install -y bat
 mkdir -p ~/.local/bin
 ln -s /usr/bin/batcat ~/.local/bin/bat
 
-# alternative ls - exa
+# ls -> exa
 wget -O exa.zip https://github.com/ogham/exa/releases/download/v0.9.0/exa-linux-x86_64-0.9.0.zip
 unzip exa.zip
 sudo mv exa-linux-x86_64 /usr/local/bin
 
+# df -> pydf
+sudo apt install -y pydf
