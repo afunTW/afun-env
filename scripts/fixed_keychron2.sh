40b229df7391095b6cc59ec7d2723f5d01481e06
afunTW <afun@afun.tw>
2022-08-28 14:34:15 +0800

    refacotr: fixed keychron by change udev

--- scripts/fixed_keychron2.sh
+++ scripts/fixed_keychron2.sh
@@ -1,17 +1,7 @@
 #!/bin/bash
 
-# https://github.com/adam-savard/keyboard-function-keys-linux
-sudo tee -a /etc/systemd/system/keychron.service > /dev/null <<EOT
-[Unit]
-Description=Disables media keys for the Keychron K2 and enables function keys
-
-[Service]
-Type=oneshot
-ExecStart=/bin/bash -c "echo 0 > /sys/module/hid_apple/parameters/fnmode"
-
-[Install]
-WantedBy=multi-user.target
+# https://github.com/IulianDita/keychron/blob/master/k2.md
+sudo tee /etc/udev/rules.d/80-keychron.rules > /dev/null <<EOT
+SUBSYSTEMS=="input", ATTRS{name}=="Keychron K2", RUN+="echo 0 | tee /sys/module/hid_apple/parameters/fnmode"
 EOT
-
-systemctl enable keychron
-systemctl start keychron
+sudo udevadm control --reload-rules && sudo udevadm trigger
